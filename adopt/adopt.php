0b782ea9a04046bf737a0bc13f69319c936297a6
Iqbalibrada <iqbalgalaxy2005@gmail.com>
2025-12-13 19:03:44 +0700

    -

--- adopt/adopt.php
+++ adopt/adopt.php
@@ -12,10 +12,12 @@ require_once '../config.php';
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Kreon:wght@300..700&family=Poppins:wght@100;300;400;700&display=swap"
-      rel="stylesheet"
-    />
+    <link 
+      href="https://fonts.googleapis.com/css2?family=Kavoon&family=Kreon:wght@300;400;500;600;700&display=swap" 
+      rel="stylesheet">
+    <link 
+      rel="stylesheet" 
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
     <!-- Feather Icon -->
     <script src="https://unpkg.com/feather-icons"></script>
